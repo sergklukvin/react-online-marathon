bde7941a75e466884c9a8eca9d36b35b18d0ccb7
sergklukvin <76059218+sergklukvin@users.noreply.github.com>
2020-12-22 00:26:01 +0200

    Update sprint 02.js

--- sprint 02.js
+++ sprint 02.js
@@ -1,1 +1,9 @@
+//// task 1
 
+const filterNums = (arrNum, compareNumber = 0, parameter = "greater") => {
+  return arrNum.filter((el) =>
+    parameter === "greater" ? compareNumber < el : compareNumber > el
+  );
+};
+
+//// task 2
