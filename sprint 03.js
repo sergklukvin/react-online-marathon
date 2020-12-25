1e571296c234c268c8d11393bbbc4c33ba61ef22
sergklukvin <76059218+sergklukvin@users.noreply.github.com>
2020-12-25 18:24:40 +0200

    Update sprint 03.js

--- sprint 03.js
+++ sprint 03.js
@@ -1,1 +1,25 @@
+/// test 01
 
+function getMin(arr) {
+  return Math.min.apply(null, arr);
+}
+
+/// test 02
+
+///test 03
+
+const Checker = require('./Checker.js');
+
+class Movie {
+  constructor(name, category, startShow) {
+    this.name = name;
+    this.category = category;
+    this.startShow = startShow;
+  }
+  watchMovie() {
+    return `I watch the movie ${this.name}!`;
+  }
+}
+
+const movie1 = new Movie('Titanic', 'drama', 1997);
+const movie2 = new Movie('Troya', 'historical', 2004);
