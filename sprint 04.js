9efe739587588d520fcc65d4deeae548a31d7866
sergklukvin <76059218+sergklukvin@users.noreply.github.com>
2020-12-27 23:32:41 +0200

    Update sprint 04.js

--- sprint 04.js
+++ sprint 04.js
@@ -1,6 +1,16 @@
 ///task 1
 ///task 2
 ///task 3
+
+const { getUser, getUserProfile } = require('./Helper.js');
+
+async function getAge() {
+  let objUser = await getUser();
+  let objUserProfil = await getUserProfile(objUser.id);
+  return objUserProfil.age;
+}
+
+
 ///task 4
 
 function* take(n, iterable) {
