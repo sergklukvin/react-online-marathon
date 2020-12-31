c8f1267a80da70a6b166753a690fa9e2aa2be856
sergklukvin <76059218+sergklukvin@users.noreply.github.com>
2020-12-31 22:44:06 +0200

    Update sprint 05.js

--- sprint 05.js
+++ sprint 05.js
@@ -3,6 +3,17 @@
 localStorage.setItem('login', 'Tom12!');
 localStorage.setItem('token', 'QhuR56Rw');
 
+//or
+
+let obj = {
+   login: "Tom12!",
+   token: 'QhuR56Rw'
+}
+
+for (let key in obj) {
+  localStorage.setItem(key, obj[key]);
+}
+
 ////task2
 
 let obj = {
