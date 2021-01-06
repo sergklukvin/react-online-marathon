f3b71bd4c73e3ebb8995f4d21accae7e0d5d2ac3
sergklukvin <76059218+sergklukvin@users.noreply.github.com>
2021-01-06 17:29:55 +0200

    Update sprint 06.js

--- sprint 06.js
+++ sprint 06.js
@@ -1,1 +1,5 @@
+///// https://github.com/ita-react-marathon-sprint6/task-1-tools-npm-sergklukvin
 
+///// https://github.com/ita-react-marathon-sprint6/task-2-tools-typescript-sergklukvin
+
+////  https://github.com/ita-react-marathon-sprint6/task-3-tools-gulp-sergklukvin
