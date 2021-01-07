13fab88049e9f0480699b52cf0907a7e2eb54e4e
sergklukvin <76059218+sergklukvin@users.noreply.github.com>
2021-01-07 20:10:38 +0200

    Update sprint 07.js

--- sprint 07.js
+++ sprint 07.js
@@ -1,2 +1,6 @@
 //// task1  https://github.com/ita-react-marathon-sprint7/task-1-tools-babel-sergklukvin
 
+//// task2  https://github.com/ita-react-marathon-sprint7/task-2-tools-webpack-sergklukvin
+
+
+
