3195741a0e5e8c99e94afac0446950b75b2fe8dd
sergklukvin <76059218+sergklukvin@users.noreply.github.com>
2021-01-11 19:21:17 +0200

    Create sprint 08.js

---
+++ sprint 08.js
@@ -0,0 +1,5 @@
+//// task1 https://github.com/ita-react-marathon-sprint8/sprint-8-jsx-1-sergklukvin
+
+//// task2  https://github.com/ita-react-marathon-sprint8/sprint-8-jsx-2-sergklukvin
+
+//// task3 https://github.com/ita-react-marathon-sprint8/sprint-8-style-sergklukvin
