f7b6241ea969f9a17da33def2d77ad00c322c636
sergklukvin <76059218+sergklukvin@users.noreply.github.com>
2021-01-14 19:51:13 +0200

    Create sprint 09.js

---
+++ sprint 09.js
@@ -0,0 +1,5 @@
+//// task1  https://github.com/ita-react-marathon-sprint9/task-1-react-keys-sergklukvin
+
+/// task2   https://github.com/ita-react-marathon-sprint9/task-2-react-props-sergklukvin
+
+/// task 3  https://github.com/ita-react-marathon-sprint9/task-3-react-state-and-events-sergklukvin
